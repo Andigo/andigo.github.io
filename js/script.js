c5b51196cdfd4f33f9e680e6fd4ad64ef676977d
Valentin <dreamofandroid@gmail.com>
2018-10-15 21:11:28 +0600

    portfolio first

---
+++ js/script.js
@@ -0,0 +1,42 @@
+window.onload = function(){
+	$(".header-menu").animate({
+		right: 0
+	}, 1000);
+
+  (function() {
+ 
+  "use strict";
+ 
+  var toggles = document.querySelectorAll(".c-hamburger");
+ 
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+ 
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  }
+ 
+})();
+
+$('.footer-copyright span').html(new Date().getFullYear());
+
+$(document).on("scroll", function(){
+        console.log($("html, body").scrollTop());
+        if($("html, body").scrollTop() > 250){
+          $(".up").fadeIn(400);
+        }else{
+          $(".up").fadeOut(400);
+        }
+      })
+      $(".up").on("click", function(){
+        $("html, body").animate({
+          scrollTop: 0
+        }, 400)
+      })
+}
+
