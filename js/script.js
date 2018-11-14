c036cafcadb5106af78dd603eb1177e1e65d6087
Valentin <dreamofandroid@gmail.com>
2018-11-14 16:58:28 +0600

    mobile menu added

--- js/script.js
+++ js/script.js
@@ -3,43 +3,20 @@ window.onload = function(){
 		right: 0
 	}, 1000);
 
-  (function() {
- 
-<<<<<<< HEAD
   "use strict"; 
- 
-  function toggleHandler(toggle) {
-    toggle.addEventListener( "click", function(e) {
-      e.preventDefault();
-      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
-    });
+$('.header-mobbtn').on('click', function(){
+  if($('.header-menu li').hasClass('open')){
+    $('.header-menu li').slideUp(300).removeClass('open');
+  }else {
+    $('.header-menu li').slideDown(300).addClass('open');
   }
-=======
-  "use strict";
- 
-   $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-    $(this).toggleClass('open');
-  });
->>>>>>> 4baeb905200ec077d2bb4c3a7c8058b0d4ebda10
- 
-})();
+})
 
 $('.footer-copyright span').html(new Date().getFullYear());
 
-$(document).on("scroll", function(){
-        console.log($("html, body").scrollTop());
-        if($("html, body").scrollTop() > 250){
-          $(".up").fadeIn(400);
-        }else{
-          $(".up").fadeOut(400);
-        }
-      })
-      $(".up").on("click", function(){
-        $("html, body").animate({
-          scrollTop: 0
-        }, 400)
-      })
+
 }
+
 
 
 
