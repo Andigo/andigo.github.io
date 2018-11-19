c077d6502a89f99300bd007b15a90deacda8bc12
Valentin <dreamofandroid@gmail.com>
2018-11-19 20:46:44 +0600

    portfolio images added

--- js/script.js
+++ js/script.js
@@ -10,6 +10,15 @@ $('.header-mobbtn').on('click', function(){
   }else {
     $('.header-menu li').slideDown(300).addClass('open');
   }
+});
+
+$('.portfolio .portfolio-items a').on('mouseover', function(){
+	$(this).find('figcaption').hide(300);
+	$(this).find('span').show(300);
+})
+$('.portfolio .portfolio-items a').on('mouseout', function(){
+	$(this).find('figcaption').show(300);
+	$(this).find('span').hide(300);
 })
 
 $('.footer-copyright span').html(new Date().getFullYear());
