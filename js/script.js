193af497d17a20e29255125124091e89612d6740
Valentin <dreamofandroid@gmail.com>
2018-11-26 16:57:26 +0600

    footer added

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.onload = function(){
 		right: 0
 	}, 1000);
 
-  "use strict"; 
+"use strict"
 $('.header-mobbtn').on('click', function(){
   if($('.header-menu li').hasClass('open')){
     $('.header-menu li').slideUp(300).removeClass('open');
@@ -14,7 +14,18 @@ $('.header-mobbtn').on('click', function(){
 
 $('.footer-copyright span').html(new Date().getFullYear());
 
-
+$(document).on("scroll", function(){
+        if($("html, body").scrollTop() > 250){
+          $(".up").fadeIn(400);
+        }else{
+          $(".up").fadeOut(400);
+        }
+      })
+      $(".up").on("click", function(){
+        $("html, body").animate({
+          scrollTop: 0
+        }, 400)
+      })
 }
 
 
